97627f7c10560741cfa8291d279035b424843c6d
Olivia-P <piganiolivia@gmail.com>
2023-06-15 14:35:01 +0200

    first budget commit

--- src/main/java/com/budgetninja/back/controller/BudgetController.java
+++ src/main/java/com/budgetninja/back/controller/BudgetController.java
@@ -1,4 +1,49 @@
 package com.budgetninja.back.controller;
 
+
+import com.budgetninja.back.model.BudgetModel;
+import com.budgetninja.back.service.BudgetService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/budget")
 public class BudgetController {
-}
+
+    private final BudgetService budgetService;
+
+    public BudgetController(BudgetService budgetService) {
+        this.budgetService = budgetService;
+    }
+
+
+
+
+    @GetMapping("")
+    public List<BudgetModel> findAll(){
+        return budgetService.findAll();
+    }
+
+    @GetMapping("{id}")
+    public BudgetModel findById(@PathVariable Long id){
+        return budgetService.findById(id);
+    }
+
+    @PostMapping("")
+    public BudgetModel save(@RequestBody BudgetModel budgetModel){
+        return budgetService.update(budgetModel);
+    }
+
+    @PutMapping("")
+    public BudgetModel update(@RequestBody BudgetModel budgetModel){
+        return budgetService.update(budgetModel);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        budgetService.deleteById(id);
+
+
+}}
--- src/main/java/com/budgetninja/back/service/BudgetService.java
+++ src/main/java/com/budgetninja/back/service/BudgetService.java
@@ -1,4 +1,46 @@
 package com.budgetninja.back.service;
 
+import com.budgetninja.back.model.BudgetModel;
+import com.budgetninja.back.repository.BudgetRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
 public class BudgetService {
+
+
+private final BudgetRepository budgetRepository;
+
+    public BudgetService(BudgetRepository budgetRepository) {
+        this.budgetRepository = budgetRepository;
+    }
+
+
+    public List<BudgetModel> findAll(){
+        return budgetRepository.findAll();
+    }
+
+
+    public BudgetModel findById(Long id){
+        return budgetRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "BUDGET not found")
+        );
+    }
+
+
+    public BudgetModel update(BudgetModel budgetModel){
+        return budgetRepository.save(budgetModel);
+    }
+
+
+
+    public void deleteById(Long id){
+        budgetRepository.deleteById(id);
+    }
+
+
+
 }
