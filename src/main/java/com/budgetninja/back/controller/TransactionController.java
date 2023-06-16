f9c147142090a50ec5ae4a0ffa050d700959f2af
Juan <juan.machado.5987@gmail.com>
2023-06-16 11:18:37 +0200

    created model, repo, service and controller for transactions

--- src/main/java/com/budgetninja/back/controller/TransactionController.java
+++ src/main/java/com/budgetninja/back/controller/TransactionController.java
@@ -1,4 +1,43 @@
 package com.budgetninja.back.controller;
 
+import com.budgetninja.back.model.TransactionModel;
+import com.budgetninja.back.service.TransactionService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/transactions")
 public class TransactionController {
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping("")
+    public List<TransactionModel> getAllTransactions() {
+        return transactionService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public TransactionModel getTransactionById(long id) {
+        return transactionService.findById(id);
+    }
+
+    @PostMapping("")
+    public TransactionModel createTransaction(@RequestBody TransactionModel transaction) {
+        return transactionService.save(transaction);
+    }
+
+    @PutMapping("/{id}")
+    public TransactionModel updateTransaction(@RequestBody TransactionModel transaction, @PathVariable long id) {
+        return transactionService.update(transaction, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTransaction(@PathVariable long id) {
+        transactionService.deleteById(id);
+    }
 }
--- src/main/java/com/budgetninja/back/repository/TransactionRepository.java
+++ src/main/java/com/budgetninja/back/repository/TransactionRepository.java
@@ -1,4 +1,9 @@
 package com.budgetninja.back.repository;
 
-public interface TransactionRepository {
+import com.budgetninja.back.model.TransactionModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<TransactionModel, Long> {
 }
