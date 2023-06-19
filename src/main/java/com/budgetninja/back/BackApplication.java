b53a74db03533f1597f6efc9db3d9c248e726990
Juan <juan.machado.5987@gmail.com>
2023-06-19 12:05:09 +0200

    added webmvcconfigurer

--- src/main/java/com/budgetninja/back/BackApplication.java
+++ src/main/java/com/budgetninja/back/BackApplication.java
@@ -2,11 +2,29 @@ package com.budgetninja.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableWebMvc
 public class BackApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("Content-Type", "Authorization");
+            }
+        };
     }
 }
