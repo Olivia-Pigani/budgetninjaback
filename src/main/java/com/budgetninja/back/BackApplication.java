eb0e3df25190650d22e220e7bbe50dbee9da7c38
duong <duongngo0103@gmail.com>
2023-06-19 19:22:35 +0200

    ajouter

--- src/main/java/com/budgetninja/back/BackApplication.java
+++ src/main/java/com/budgetninja/back/BackApplication.java
@@ -1,5 +1,4 @@
 package com.budgetninja.back;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
